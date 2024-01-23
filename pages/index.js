ff8e116beaa867241f8497989fe939b31b1753df
Gilbert Ardila <gilbertferney@gmail.com>
2024-01-23 06:01:37 -0500

    add ico and images to public folder

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Canary app</title>
+        <title>Sparrow app</title>
         <meta name="description" content="this is a Twitter clone only for educational purposes" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
