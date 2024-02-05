0587301748ef23c9357b5754456e6b91964d7c0c
Gilbert Ardila <gilbertferney@gmail.com>
2024-02-05 06:48:13 -0500

    like functionality

--- components/feed/posts/Post.jsx
+++ components/feed/posts/Post.jsx
@@ -1,13 +1,49 @@
+/* eslint-disable @next/next/no-img-element */
 import Moment from 'react-moment';
+import { collection, deleteDoc, doc, onSnapshot, setDoc } from "firebase/firestore";
+import { db } from "../../../firebase"
+import { signIn, useSession } from "next-auth/react";
+import { useEffect, useState } from 'react';
 
 
 const Post = ({ post }) => {
+  const { data: session } = useSession();
+  const [likes, setLikes] = useState([]); 
+  const [hasLiked , setHasLiked] = useState(false); 
+
+  //get the posts´likes
+  useEffect(()=>{
+    const unsubscribe = onSnapshot(collection(db,"posts",post.id,"likes"),(snapshot)=>setLikes(snapshot.docs))
+  },[db])
+   
+  //check if user has liked the post
+  useEffect(() => {
+    setHasLiked(likes.findIndex((like)=>like.id === session?.user.uid) !== -1)
+  }, [likes])
+  
+
+  async function likePost() {
+    if(session){
+      if(hasLiked){
+      await deleteDoc(doc(db, "posts", post.id, "likes", session?.user.uid))
+    }else{
+      await setDoc(doc(db, "posts", post.id, "likes", session?.user.uid),{
+      username: session?.user.username
+    })
+    }
+    }else{
+      signIn()
+    }
+    
+    
+  }
+
   return (
     <div className="flex p-3 cursor-pointer border-b border-gray-200">
 
       {/**user image */}
       <div className="flex  h-full w-16 justify-center pt-2">
-        <img  src={post.data().userImage} alt={post.data().userName} className="rounded-full min-w-11 min-h-11 " />
+        <img src={post.data().userImage} alt={post.data().userName} className="rounded-full min-w-11 min-h-11 " />
       </div>
 
       {/**posts right side*/}
@@ -21,7 +57,7 @@ const Post = ({ post }) => {
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{post.data().name}</h4>
             <span className="text-sm sm:text-[15px]">@{post.data().userName}-</span>
             <span className="text-sm sm:text-[15px] hover:underline">
-            <Moment fromNow>{post?.timestamp?.toDate()}</Moment></span>
+              <Moment fromNow>{post?.data().timestamp?.toDate()}</Moment></span>
           </div>
 
           {/**dots icon */}
@@ -39,7 +75,7 @@ const Post = ({ post }) => {
 
         {/**post image */}
 
-        <img src={post.data().image} alt={post.data().text} className="rounded-2xl mr-2 w-64" />
+        <img src={post.data().image} alt={post.data().text} className="rounded-2xl mr-2 object-content  w-full" />
 
 
         {/**tweet buttons */}
@@ -54,12 +90,28 @@ const Post = ({ post }) => {
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 hoverEffect hover:bg-red-100 hover:text-red-600 p-2">
             <path strokeLinecap="round" strokeLinejoin="round" d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0" />
           </svg>
-
-          {/**like icon */}
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 hoverEffect hover:bg-red-100 hover:text-red-500 p-2">
+          
+          {/**like icon depends on likes */}
+          <div className='flex items-center space-x-1'>
+          {hasLiked ? (
+           
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-5 h-5 text-red-600"  onClick={likePost}>
+            <path d="m11.645 20.91-.007-.003-.022-.012a15.247 15.247 0 0 1-.383-.218 25.18 25.18 0 0 1-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0 1 12 5.052 5.5 5.5 0 0 1 16.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 0 1-4.244 3.17 15.247 15.247 0 0 1-.383.219l-.022.012-.007.004-.003.001a.752.752 0 0 1-.704 0l-.003-.001Z" />
+          </svg>
+         
+        
+          
+          ):(
+             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 hoverEffect hover:bg-red-100 hover:text-red-500 p-2" onClick={likePost}>
             <path strokeLinecap="round" strokeLinejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12Z" />
           </svg>
 
+          )}
+          {/**post´s likes number */}
+          {likes.length > 0 &&( <span>{likes.length }</span>)}
+         
+          </div>
+         
           {/**share icon */}
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-9 h-9 hoverEffect hover:bg-sky-100 hover:text-sky-500 p-2">
             <path strokeLinecap="round" strokeLinejoin="round" d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z" />
