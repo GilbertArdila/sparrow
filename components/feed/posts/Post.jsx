a125aa6c67ae2f47796df77257293ff14de8da5b
Gilbert Ardila <gilbertferney@gmail.com>
2024-02-07 16:04:34 -0500

    send comments, show number of comments, redirect using useRouter

--- components/feed/posts/Post.jsx
+++ components/feed/posts/Post.jsx
@@ -13,8 +13,10 @@ import { useRecoilState } from 'recoil';
 const Post = ({ post }) => {
   const { data: session } = useSession();
   const [likes, setLikes] = useState([]);
+  const [comments, setComments] = useState([]);
   const [hasLiked, setHasLiked] = useState(false);
-  
+
+
   //recoil state to open the modal
   const [open, setOpen] = useRecoilState(modal);
   //recoil state to set the post user's data
@@ -27,10 +29,16 @@ const Post = ({ post }) => {
     const unsubscribe = onSnapshot(collection(db, "posts", post.id, "likes"), (snapshot) => setLikes(snapshot.docs))
   }, [db])
 
+  //get the posts´comments
+  useEffect(() => {
+    onSnapshot(collection(db, "posts", post.id, "comments"), (snapshot) => setComments(snapshot.docs))
+  }, [])
+
   //check if user has liked the post
   useEffect(() => {
     setHasLiked(likes.findIndex((like) => like.id === session?.user.uid) !== -1)
   }, [likes])
+
 
   //like post function
   async function likePost() {
@@ -103,27 +111,43 @@ const Post = ({ post }) => {
         {/**tweet buttons */}
         <div className="flex items-center  justify-between text-gray-500 p-2">
 
-          {/**chat icon */}
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-9 h-9 hoverEffect hover:bg-sky-100 hover:text-sky-500 p-2"
-            onClick={() => {
-              if (!session) {
-                signIn();
-              } else {
-                setOpen(!open);
-                setUserData(post.data());
-                setActualPostId(post.id);
-              }
 
-            }}
-          >
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
-          </svg>
+          <>
+            {session?.user.uid !== post.data().id ? (
+              <>
+                {/**chat icon */}
+                <div className='flex  items-center justify-center space-x-1'>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth={1.5}
+                    stroke="currentColor"
+                    className="w-9 h-9 hoverEffect hover:bg-sky-100 hover:text-sky-500 p-2"
+                    onClick={() => {
+                      if (!session) {
+                        signIn();
+                      } else {
+                        setOpen(!open);
+                        setUserData(post.data());
+                        setActualPostId(post.id);
+                      }
+
+                    }}
+                  >
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+                  </svg>
+                  {comments.length > 0 && <span>{comments.length}</span>}
+                </div>
+              </>
+            ): <>
+            {comments.length > 0 && <span >N° comments: {comments.length}</span>}
+            </>}
+
+          </>
+
+
+
 
           {/**trash icon only if user is the post´s owner */}
           {session?.user.uid === post.data().id && (
--- components/modal/CommentsModal.jsx
+++ components/modal/CommentsModal.jsx
@@ -4,8 +4,10 @@ import { useRecoilState } from 'recoil';
 import { modal, postUser, postId } from '../../globalStates/atom';
 import { useSession } from 'next-auth/react';
 import Moment from 'react-moment';
-import { useRef, useState } from 'react';
-
+import {  useState } from 'react';
+import { collection, addDoc, serverTimestamp } from 'firebase/firestore';
+import { db } from '../../firebase';
+import { useRouter } from 'next/router';
 
 
 const CommentsModal = () => {
@@ -17,35 +19,42 @@ const CommentsModal = () => {
 
   const { data: session } = useSession();
   const [inputData, setInputData] = useState("");
-  const [imageFile, setImageFile] = useState(null);
-  const imageFilePicker = useRef(null);
+  const router = useRouter();
+ 
 
-
+  
 
   //close  modal function
   const closeModal = () => {
     setOpenModal(false);
   }
 
-  //get and store image url function
-  const addImageFile = (e) => {
-
-    const reader = new FileReader();
-    if (e.target.files[0]) {
-        reader.readAsDataURL(e.target.files[0]);
-    }
-
-    reader.onload = (readerEvent) => {
-        setImageFile(readerEvent.target.result)
-    }
-}
-
-//send comment function
-async function sendComment() {
   
+  //send comment function
+  async function sendComment() {
+    if(actualPostId === session.user.uid) return;
+    else{
+      await addDoc(collection(db, "posts", actualPostId, "comments"), {
+      comment: inputData,
+      name: session.user.name,
+      userName: session.user.username,
+      userImage: session.user.image,
+      timestamp: serverTimestamp(),
+      id:session.user.uid
+      
+    });
+
+    setOpenModal(false);
+    setInputData("");
+    //router.push(`posts/${actualPostId}`)
+    }
+    
+
+  }
+
+   
 
  
-}
 
   return (
     <div>
@@ -69,67 +78,60 @@ async function sendComment() {
           <div className='p-2 flex items-center space-x-1 relative'>
             {/**this span is the connector line between both pictures */}
             <span className='w-0.5 h-full z-[-1] absolute left-8 top-11 bg-gray-300' />
-            
+
             <img src={actualPostUser?.userImage} alt={actualPostUser?.userName} className="rounded-full w-11 h-11 " />
             <h4 className="font-bold text-[15px] sm:text-[16px] hover:underline">{actualPostUser?.name}</h4>
             <span className="text-sm sm:text-[15px]">@{actualPostUser?.userName}-</span>
             <span className="text-sm sm:text-[15px] hover:underline">
               <Moment fromNow>{actualPostUser.timestamp?.toDate()}</Moment></span>
           </div>
-           <p className='text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2'>{actualPostUser?.text}</p>
+          <p className='text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2'>{actualPostUser?.text}</p>
 
-            {/**user´s data */}
-            <div className="flex  p-3 space-x-3">
-              {/**profile image */}
-              <img
-                src={session?.user.image}
-                alt={session?.user.name}
-                className="rounded-full w-11 h-11 object-contain cursor-pointer hover:brightness-95" />
+          {/**user´s data */}
+          <div className="flex  p-3 space-x-3">
+            {/**profile image */}
+            <img
+              src={session?.user.image}
+              alt={session?.user.name}
+              className="rounded-full w-11 h-11 object-contain cursor-pointer hover:brightness-95" />
 
-                {/**input and icons */}
-              <div className="w-full divide-y divide-gray-200">
-                <div >
-                  <textarea
-                    rows="2"
-                    placeholder="Write your reply"
-                    className="w-full focus:ring-0 border-none text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 border-b "
-                    value={inputData}
-                    onChange={(e) => setInputData(e.target.value)}></textarea>
-                </div>
+            {/**input and icons */}
+            <div className="w-full divide-y divide-gray-200">
+              <div >
+                <textarea
+                  rows="2"
+                  placeholder="Write your reply"
+                  className="w-full focus:ring-0 border-none text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700 border-b "
+                  value={inputData}
+                  onChange={(e) => setInputData(e.target.value)}></textarea>
+              </div>
 
-                <div className="flex items-center justify-between pt-2.5">
-                      {/**icons */}
-                      <div className="flex items-center">
-                        {/**Photo icon and file input */}
-                        <div className="" onClick={() => imageFilePicker.current.click()}>
-                          {inputData.trim() && (
-                            <>
-                              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={`w-10 h-10 hoverEffect p-2 text-sky-500 ${inputData.trim() ? 'hover:bg-sky-100' : 'hover:bg-red-200'} ${imageFile ? 'bg-green-300' : null}`}>
-                                <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z" />
-                              </svg>
-                              <input type="file" hidden ref={imageFilePicker} onChange={addImageFile} />
-                            </>
-                          )}
-                        </div>
-
-                        {/**Emoji icon */}
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100">
-                          <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
-                        </svg>
-                      </div>
-
-                      {/**send post button */}
-                      <button
-                        disabled={!inputData.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold hover:brightness-95 shadow-md disabled:opacity-50"
-                        onClick={sendComment}
-                      >Reply</button>
+              <div className="flex items-center justify-between pt-2.5">
+                {/**icons */}
+                <div className="flex items-center">
+                  {/**Photo icon and file input */}
+                  <div className="" onClick={() => imageFilePicker.current.click()}>
                     
+                  </div>
 
+                  {/**Emoji icon */}
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10 p-2 hoverEffect text-sky-500 hover:bg-sky-100">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M15.182 15.182a4.5 4.5 0 0 1-6.364 0M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+                  </svg>
                 </div>
+
+                {/**send post button */}
+                <button
+                  disabled={!inputData.trim()} className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold hover:brightness-95 shadow-md disabled:opacity-50"
+                  onClick={sendComment}
+                >Reply</button>
+
 
               </div>
+
             </div>
-          
+          </div>
+
 
         </div>
 
